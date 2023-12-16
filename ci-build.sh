0d7f629e8823ffe0457dbc745568f12eeabb02d7
halal-beef <78730004+halal-beef@users.noreply.github.com>
2023-12-16 14:18:38 +0000

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc mkbootimg
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2 --recursive -b edk2-stable202302
-git clone https://github.com/tianocore/edk2-platforms.git
+git clone https://github.com/tianocore/edk2 --recursive -b edk2-stable202302 --depth=1
+git clone https://github.com/tianocore/edk2-platforms.git --depth=1
 cd "$curdir"
 ./firstrun.sh
 ./build.sh
